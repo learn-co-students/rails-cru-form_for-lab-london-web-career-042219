54d68bda6339d554cbfc2b0ed94ee85f82557959
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-18 23:36:57 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists
+  get '/welcome', to: 'welcome#index', as: 'welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
